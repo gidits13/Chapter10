4dfa09eacb949a42c42cbac1f5a16489e58cf0a9
gidits13 <gidits13@gmail.com>
2022-09-17 19:39:04 +0300

    Задание 10.3.1

---
+++ p31/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace p31
+{
+    public interface IWriter
+    {
+        void Write();
+    }
+
+    public interface IReader
+    {
+        void Read();
+    }
+
+    public interface IMailer
+    {
+        void SendEmail();
+    }
+
+    public class FileManager : IWriter, IReader, IMailer
+    {
+        void IReader.Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IMailer.SendEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IWriter.Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
